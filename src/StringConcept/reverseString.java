87522e01cc31a268cfa0d3984d386a8f9b430c73
Gnanasekar RS <56383255+IndiumsoftBajajFinserv@users.noreply.github.com>
2020-02-04 16:54:41 +0530

    String immutable

---
+++ src/StringConcept/reverseString.java
@@ -0,0 +1,62 @@
+package StringConcept;
+
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class reverseString {
+
+    @Test
+    public void reverse()
+    {
+        StringBuffer buffer = new StringBuffer("Automation");
+        System.out.println(buffer.reverse());
+    }
+
+
+    @Test
+    public void reverseusingBuilder()
+    {
+        StringBuilder build = new StringBuilder("AutomationBuilder");
+        System.out.println(build.reverse());
+    }
+
+    @Test
+    public void usingChar()
+    {
+        String x =  "Automation Reverse using char";
+        char lis[] = x.toCharArray();
+        int size = lis.length;
+        for(int i = size-1; i>=0; i--)
+        {
+            System.out.print(lis[i]);
+        }
+    }
+
+
+    @Test
+    public void scannerFnctionality()
+    {
+        String str;
+        //Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the String to be reverse");
+      //  str = obj.nextLine();
+        str = "Gnanasekar";
+
+        String listofCharacters[] = str.split("");
+        for(int i = listofCharacters.length-1 ; i>=0;i--)
+        {
+            System.out.print(listofCharacters[i]);
+        }
+    }
+
+    @Test
+    public void simple()
+    {
+        String c = "ADF";
+        for(int i = c.length()-1; i >=0;i--)
+        {
+            System.out.print(c.charAt(i));
+        }
+    }
+}
--- src/stringConcepts.java
+++ src/stringConcepts.java
@@ -3,11 +3,39 @@ public class stringConcepts {
     public static void stringvs(String s1)
     {
       s1 = s1+ "Sekar";
+    //  s1.concat("Sekar");
     }
 
-    public  static void main(String args[])
+
+    public static void stringBufferingConcept(StringBuffer s2)
     {
-       String s1 = "Gnana";
+        s2.append(" Sekar");
+    }
+
+
+    public static void stringBuilderConcept(StringBuilder s3)
+    {
+        s3.append("ji");
+    }
+
+    public static void main(String args[])
+    {
+       String s1 = "Gnana";   //String is Immutable
        stringvs(s1);
+       System.out.println(s1);
+
+       StringBuffer sbuffer = new StringBuffer("Gnana");  //String Buffer is Immutable, single Thread
+       stringBufferingConcept(sbuffer);
+        System.out.println(sbuffer);
+
+        StringBuffer sbuffer2 = new StringBuffer("Rakesh");  //String Buffer is Immutable, single Thread
+        stringBufferingConcept(sbuffer2);
+        System.out.println(sbuffer2);
+
+        StringBuilder sbuilder = new StringBuilder("Sekar "); //String Builder is Immutable . Multi thread
+        stringBuilderConcept(sbuilder);
+        System.out.println(sbuilder);
+       // System.out.println(obj.append("sekarji"));
+
     }
 }
