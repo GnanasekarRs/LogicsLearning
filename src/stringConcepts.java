395ca6789d3f1e92634dca878cdab0893da531d9
Gnanasekar RS <56383255+IndiumsoftBajajFinserv@users.noreply.github.com>
2020-02-04 13:22:19 +0530

    String immutable

--- src/stringConcepts.java
+++ src/stringConcepts.java
@@ -1,6 +1,13 @@
 public class stringConcepts {
+
+    public static void stringvs(String s1)
+    {
+      s1 = s1+ "Sekar";
+    }
+
     public  static void main(String args[])
     {
-
+       String s1 = "Gnana";
+       stringvs(s1);
     }
 }
